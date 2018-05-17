1c16a0ffaf5933c9c7c78a2711f2199827f508ef
zhuzhenguang <zhuzhenguang2008@gmail.com>
2018-05-17 22:48:28 +0800

    some collection func

--- src/main/scala/CollectionPractice.scala
+++ src/main/scala/CollectionPractice.scala
@@ -24,5 +24,33 @@ object CollectionPractice {
       case Some(n) => println(n)
       case None => println("None")
     }
+
+    println(numberList.map(i => i * 2))
+
+    def timeTwo(i: Int) = i * 2
+
+    println(numberList.map(timeTwo))
+
+    println(numberList.filter(i => i % 2 == 0))
+
+    def isEven(i: Int) = i % 2 == 0
+
+    println(numberList.filter(isEven))
+
+    println(List(1, 2, 3).zip(List("a", "b", "c")))
+
+    println(numberList.partition(i => i % 2 == 0))
+
+    println(numberList.find(i => i > 5))
+
+    println(numberList.drop(2))
+
+    println(numberList.dropWhile(i => i < 3))
+
+    println(numberList.foldLeft(0)((m, n) => m + n))
+
+    println(List(List(1, 2, 3), List(4, 5)).flatten)
+
+    println(List(List(1, 2, 3), List(4, 5)).flatMap(i => i.map(_ * 2)))
   }
 }
